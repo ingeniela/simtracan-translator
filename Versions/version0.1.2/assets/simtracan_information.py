abe0ed48361f8e9baa66aa9a5a336d436042a656
Daniela Bai <112042907+danielabai@users.noreply.github.com>
2022-10-25 13:52:40 -0400

    Update simtracan_information.py
    Update of "version improvements"

--- Versions/version0.1.2/assets/simtracan_information.py
+++ Versions/version0.1.2/assets/simtracan_information.py
@@ -1,11 +1,12 @@
 # Version
 version_now = '0.1.2'
-version_improvements = []
+author_comment = 'Seeing the code difference between 0.1.1 and 0.1.2 makes me very happy. It can really how much I have improved as a programmer and I hope in a few years I can look back to these kinds of projects and see how much I have advanced. Thank you for using this software:).'
+version_date = 'October 11, 2022' #This version was published on October 11, 2022.
+version_improvements = ['Better input for text', 'Better dictionaries for translation', 'New and better functions for translation', 'OOP improvement', 'Lighter code', 'Cleaner code', 'Addition of different folders and files for the translator']
 
 # Now
 class info:
     author_name = 'Daniela Bai'
-    author_email = 'danielabai888@gmail.com'
     author_github = 'danielabai'
     setup_name = 'Simtracan Translator'
     setup_requires = 'Python Vanilla'
@@ -22,3 +23,4 @@ class info:
         'Topic :: Education',
     ]
     setup_github = "www.github.com/danielabai/simtracan-translator"
+    
